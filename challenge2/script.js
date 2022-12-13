d38a1651b858bc58d23a314c9b00619f360ca341
James Adams <Jadams2013@gmail.com>
2022-12-12 19:50:03 -0700

    Mostly complete

--- challenge2/script.js
+++ challenge2/script.js
@@ -1,4 +1,4 @@
-
+//Keyboard stuff
 window.addEventListener('keyup', keyUp);
 
 document.onkeydown = keyDown;
@@ -6,6 +6,18 @@ document.onkeydown = keyDown;
 
 function keyDown(e) {
     document.getElementById("display").innerHTML = e.code;
+
+    if (document.getElementById("keyboard").hidden) {
+        document.getElementById("keyboard").hidden = false;
+        document.getElementById("mainKeys").hidden = false;
+        document.getElementById("navKeys").hidden = false;
+        document.getElementById("keyboardNote").hidden = true;
+    }
+    if (document.getElementById("numpad").hidden) { 
+        if (e.code.slice(0,3) === "Num" || e.getModifierState("NumLock")) {
+            document.getElementById("numpad").hidden = false;
+        }
+    }
     const key = document.querySelector(`div[data-key="${e.code}"]`);
     if (!key) return;
     key.classList.add('down');
@@ -20,10 +32,11 @@ function keyUp(e) {
 }
 
 
-
+//mouse stuff
 window.addEventListener('mousedown', mouseDown);
 
 function mouseDown(e) {
+    mouseVisible();
     document.getElementById("display").innerHTML = 'Mouse'+e.button;
     const key = document.querySelector(`div[data-key="Mouse${e.button}"]`);
     if (!key) return;
@@ -39,10 +52,10 @@ function mouseUp(e) {
     key.classList.add('pressed');
 }
 
-
 window.addEventListener('wheel', mouseScroll);
 
 function mouseScroll(e) {
+    mouseVisible();
     document.getElementById("display").innerHTML = 'MouseWheel'+e.deltaY;
     if(e.deltaY < 0) {
         const key = document.querySelector(`div[data-key="MouseWheelUp"]`);
@@ -62,9 +75,76 @@ function mouseScroll(e) {
     }
 }
 
+function mouseVisible() {
+    if (document.getElementById("mouse").hidden) {
+        document.getElementById("mouseNote").hidden = true;
+        document.getElementById("mouse").hidden = false;
+        document.getElementById("mouseSub").hidden = false;
+    }
+}
 
 
-
+//allows delays 
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
+}
+
+
+//Light and dark mode
+var r = document.querySelector(':root');
+
+function swapMode(e) {
+    if (e.innerHTML == "Swap to Light Mode") lightMode(e);
+    else darkMode(e);
+}
+function lightMode(e) {
+    r.style.setProperty('--background', 'white');
+    r.style.setProperty('--text', 'black');
+    r.style.setProperty('--header', '#bfbfbf');
+    e.innerHTML = "Swap to Dark Mode";
+}
+function darkMode(e) {
+    r.style.setProperty('--background', '#1a1a1a');
+    r.style.setProperty('--text', '#ffffff');
+    r.style.setProperty('--header', '#404040');
+    e.innerHTML = "Swap to Light Mode";
+}
+
+
+//reset page 
+function clearKeys() {
+    document.getElementById("display").innerHTML = "---";
+    const keys = Array.from(document.querySelectorAll('.key'));
+    keys.forEach(key => key.classList.remove('down'));
+    keys.forEach(key => key.classList.remove('pressed'));
+}
+
+
+//Gamepad stuff
+window.addEventListener('gamepadconnected', updateGamepad);
+
+function updateGamepad() {
+    var gamepads = navigator.getGamepads();
+    gamepads.forEach(gamepad => {
+        if (document.getElementById(gamepad.index+"Gamepad").hidden) {
+            document.getElementById("gamepadNote").hidden = true;
+            document.getElementById(gamepad.index+"Gamepad").hidden = false;
+            document.getElementById(gamepad.index+"GamepadSub").hidden = false;
+        }
+        gamepad.buttons.forEach( (button, i) => {
+            var key = document.querySelector(`div[data-key="${gamepad.index}Gamepad${i}"]`);
+            if (!key) return;
+            if (button.pressed) {
+                if (!key.classList.contains("down")) {
+                    key.classList.add("down"); 
+                    document.getElementById("display").innerHTML = gamepad.index + "Gamepad" + i;
+                }
+            }
+            else if (key.classList.contains("down")) {
+                key.classList.add("pressed");
+                key.classList.remove("down");
+            }
+        });
+    });
+    delay(50).then(() => updateGamepad());
 }
